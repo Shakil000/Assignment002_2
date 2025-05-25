6eb42619e768284decf9ab821942e3cd1d5bb6df
Shakil000 <racer.shakil111@gmail.com>
2025-05-25 07:36:14 +0600

    insert values done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -29,4 +29,34 @@ CREATE TABLE sightings(
     note TEXT
 );
 
+--! Insert data to Rangers Table---------
 
+INSERT INTO rangers(name,region)
+VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+SELECT * from rangers;
+
+--! Insert data to Species Table---------
+
+INSERT INTO species(common_name,scientific_name,discovery_data,conservation_status)
+VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+SELECT * FROM species;
+
+--! Insert data to Sightings Table---------
+
+INSERT INTO sightings(ranger_id,species_id,location,sighting_time,note)
+VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * from sightings;
