9a31c90ea019c2897bdd50e496b8c98097a008a6
Shakil000 <racer.shakil111@gmail.com>
2025-05-25 19:48:13 +0600

    problem eight solve

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -100,6 +100,13 @@ UPDATE species
 SET conservation_status = 'Historic'
 WHERE discovery_data < '1800-01-01';
 
-SELECT * FROM species;
+--! Problem 8️⃣ Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
 
+SELECT sighting_id,
+CASE
+    WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+    ELSE 'Evening'
+END AS time_of_day    
+FROM sightings;
 
